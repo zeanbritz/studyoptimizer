58f4ac75752a42f559a3e4cee788e23a2e78f6b4
Zazza <zd.britz@icloud.com>
2026-08-07 16:33:02 +0200

    apps created

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "accounts",
+    "dashboard",
+    "subjects",
+    "exams",
+    "planner",
+    "practice",
 ]
 
 MIDDLEWARE = [
