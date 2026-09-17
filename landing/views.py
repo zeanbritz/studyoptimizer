18c96067162bf97868d294c83a88adcc59706cb4
Zazza <zd.britz@icloud.com>
2026-09-17 23:10:31 +0200

    landing toolbar

--- landing/views.py
+++ landing/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "landing/home.html")
+
+
+def pricing(request):
+    return render(request, "landing/pricing.html")
