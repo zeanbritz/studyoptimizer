02ceb396452fc0846389c5bc3b181568f6b31861
Zazza <zd.britz@icloud.com>
2026-09-17 23:36:44 +0200

    footer added

--- landing/urls.py
+++ landing/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.home, name="landing"),
     path("pricing/", views.pricing, name="pricing"),
+    path("privacy/", views.privacy_policy, name="privacy_policy"),
+    path("terms/", views.terms_of_use, name="terms_of_use"),
+    path("cookies/", views.cookie_policy, name="cookie_policy"),
+    path("billing/", views.billing_policy, name="billing_policy"),
+    path("contact/", views.contact, name="contact"),
 ]
--- landing/views.py
+++ landing/views.py
@@ -7,3 +7,23 @@ def home(request):
 
 def pricing(request):
     return render(request, "landing/pricing.html")
+
+
+def privacy_policy(request):
+    return render(request, "landing/legal/privacy.html")
+
+
+def terms_of_use(request):
+    return render(request, "landing/legal/terms.html")
+
+
+def cookie_policy(request):
+    return render(request, "landing/legal/cookies.html")
+
+
+def billing_policy(request):
+    return render(request, "landing/legal/billing.html")
+
+
+def contact(request):
+    return render(request, "landing/contact/contact.html")
